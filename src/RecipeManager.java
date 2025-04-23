cb6970d62999f1aa459bdb78322197120af141f5
plskyll <118909309+plskyll@users.noreply.github.com>
2025-04-23 14:44:20 +0300

    Update RecipeManager.java

--- src/RecipeManager.java
+++ src/RecipeManager.java
@@ -1,22 +1,20 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
 
 public class RecipeManager {
     private List<Recipe> recipes = new ArrayList<>();
     Scanner scanner = new Scanner(System.in);
 
     public void addRecipe() {
-        System.out.println("Додайте рецепт");
-        System.out.println("\nНазва: ");
+        System.out.println("\nДодайте рецепт");
+        System.out.println("Назва: ");
         String name = scanner.nextLine();
-        System.out.println("Інгредієнти: ");
+        System.out.println("Інгредієнти (через кому): ");
         List<String> ingredients = new ArrayList<>(Arrays.asList(scanner.nextLine().split(",")));
         System.out.println("Час приготування: ");
         int timeCook = scanner.nextInt();
         scanner.nextLine();
         recipes.add(new Recipe(name, timeCook, ingredients));
+        System.out.println("Рецепт " + name + " успішно додано\n");
     }
 
     public void editRecipe() {
@@ -42,7 +40,7 @@ public class RecipeManager {
         recipe.setName(name);
         recipe.setIngredients(new ArrayList<>(ingredients));
         recipe.setTimeCook(cookTime);
-        System.out.println("Рецепт оновлено.");
+        System.out.println("Рецепт оновлено.\n");
     }
 
     public void deleteRecipe(){
@@ -54,20 +52,62 @@ public class RecipeManager {
             System.out.println("Невірний номер.");
             return;
         }
-
-        System.out.println("Рецепт з ID " + index + " видалено.");
+        String name = recipes.get(index - 1).getName();
+        System.out.println("Рецепт " + name + " з ID " + index + " видалено.\n");
         recipes.remove(index - 1);
 
     }
-    public void showRecipes(){
-        System.out.println("Всі рецепти: ");
-        for (int i = 0; i < recipes.size(); i++) {
-            System.out.println(recipes.get(i).getId());
-            System.out.println(recipes.get(i).getName());
-            System.out.println(recipes.get(i).getTimeCook());
-            System.out.println(recipes.get(i).getIngredients());
 
-            System.out.println();
+    public void filterRecipe() {
+        System.out.println("Введіть час для фільтрації: ");
+        int time = scanner.nextInt();
+        scanner.nextLine();
+
+        System.out.println();
+
+        int counter = 0;
+        for (int i = 0; i < recipes.size() ; i++) {
+            if (recipes.get(i).getTimeCook() == time){
+                recipes.get(i).showRecipe();
+                counter++;
+            }
+        }
+
+        if (counter == 0){
+            System.out.println("\nЗа цим фільтром рецептів не знайдено.\n");
+        }
+    }
+
+    public void sortRecipes(){
+        System.out.println("Оберіть метод сортування:");
+        System.out.println("a. За назвою");
+        System.out.println("b. За часом приготування");
+        System.out.println("c. За кількістю інгредієнтів");
+        System.out.print("Ваш вибір: ");
+
+        String choiceStr = scanner.nextLine();
+
+        if (choiceStr.equalsIgnoreCase("a")) {
+            recipes.sort(Comparator.comparing(Recipe::getName));
+            showRecipes();
+        } else if (choiceStr.equalsIgnoreCase("b")) {
+            recipes.sort(Comparator.comparing(Recipe::getTimeCook));
+            showRecipes();
+        } else if (choiceStr.equalsIgnoreCase("c")) {
+            recipes.sort(Comparator.comparing(Recipe::getQuantityIngredients));
+            showRecipes();
+        } else {
+            System.out.println("Невірний вибір.");
+        }
+
+        recipes.sort(Comparator.comparing(Recipe::getId)); //вертаю в початковий стан
+    }
+
+
+    public void showRecipes(){
+        System.out.println("\nВсі рецепти: ");
+        for (int i = 0; i < recipes.size(); i++) {
+            recipes.get(i).showRecipe();
         }
     }
 }
